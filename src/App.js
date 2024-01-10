4f0ed861f51dfc282c08d08a44ed79b2542d228b
Victor <victornuneshvh10@gmail.com>
2024-01-10 10:14:26 -0300

    game class complet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import Footer from './components/generics/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* Cabeçalho */}
+      <header>
+        <h1>Meu jogos nostálgicos</h1>
       </header>
+      {/* As caixas com os jogos (grid) */}
+      <main>
+        <section className='games-container'>
+          <div id="mario" className={styles.game}>
+          <img src="https://pngimg.com/uploads/mario/mario_PNG7.png" alt="mario" className={styles.img} />
+            <h3>Nintendo - Mario</h3>
+            <p>Um dos jogos que mais joguei...</p>
+          </div>
+          <div id="san-andreas" className={styles.game}>
+            <img src="https://pngimg.com/uploads/gta/gta_PNG12.png" alt="san-andreas" className={styles.img} />
+            <h3>Nintendo - Mario</h3>
+            <p>Um dos jogos que mais joguei...</p>
+          </div>
+          <div id="crash" className={styles.game}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBLawPrAwfjlXhDOMehOXVv2DTMHWd7L_a5g&usqp=CAU" alt="crash" className={styles.img} />
+            <h3>Nintendo - Mario</h3>
+            <p>Um dos jogos que mais joguei...</p>
+          </div>
+          <div id="minecraft" className={styles.game}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBLawPrAwfjlXhDOMehOXVv2DTMHWd7L_a5g&usqp=CAU" alt="minecraft" className={styles.img} />
+            <h3>Nintendo - Mario</h3>
+            <p>Um dos jogos que mais joguei...</p>
+          </div>
+        </section>
+      </main>
+      {/* Footer. Dx em componente, pois vai entrar em mts lugares */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/generics/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.module.css';
+
+function Footer () {
+    return (
+        <footer>
+        <p>
+          <span className='bold'>Jogos mais jogados - By Victor Freitas</span> &copy; 2024 
+        </p>
+      </footer> 
+    )
+}
+
+export default Footer;
